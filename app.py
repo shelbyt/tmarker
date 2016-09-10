2869dd62888cb44c206378a5e6c684c7d79a01af
shelbyt <sht005@eng.ucsd.edu>
2016-09-10 12:39:15 -0700

    Initial commit of tmarker server

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+from flask import request
+
+
+
+@app.route('/', methods=['GET','POST'])
+def json():
+    app.logger.debug("JSON received...")
+    app.logger.debug(request.json)
+
+    if request.json:
+        mydata = request.json
+
+        return "name is = %s\n" % mydata.get("name")
+    else:
+        return "no json received"
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ command.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json;"  http://127.0.0.1:5000/ -X POST -d '{"name":"shelby"}'
