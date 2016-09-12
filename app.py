86a965f6f49731f098845d6bfc92745e55fc8f28
shelbyt <sht005@eng.ucsd.edu>
2016-09-11 20:56:18 -0700

    Basic support to receive name/id and dl sub file
    App server is able to process a json query of the type
    {name:x, id:y} where name is the name of the video and id is
    the youtube id.
    
    This id is then passed to a function that downloads the subfile
    as a ttml and subsequently does preprocessing + outputs the
    file with the name "$video_id".

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
+from __future__ import unicode_literals
 from flask import Flask
 app = Flask(__name__)
 from flask import request
+import youtube_dl
+import HTMLParser
+parser = HTMLParser.HTMLParser()
 
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+ydl_opts = {
+    'writeautomaticsub': True,
+    'skip_download': True,
+    'subtitlesformat': 'ttml',
+    'outtmpl': 'output',
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+def preProcessAndOutput(video_id):
+    outfile_name = str(video_id)
+    with open('output.en.ttml') as infile, open(outfile_name,'w') as outfile:
+        for line in infile:
+            outfile.write(parser.unescape(line))
+
+def downloadSubs(video_id):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(['http://www.youtube.com/watch?v='+str(video_id)])
+        preProcessAndOutput(video_id)
 
 @app.route('/', methods=['GET','POST'])
 def json():
@@ -10,8 +49,9 @@ def json():
 
     if request.json:
         mydata = request.json
-
+        downloadSubs(mydata.get("id"))
         return "name is = %s\n" % mydata.get("name")
+        return "ytid is = %s\n" % mydata.get("id")
     else:
         return "no json received"
 
--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/json;"  http://127.0.0.1:5000/ -X POST -d '{"name":"shelby"}'
+curl -H "Content-Type: application/json;"  http://127.0.0.1:5000/ -X POST -d '{"name":"shelby", "id":"9DUQ7_7Pj_c"}'
