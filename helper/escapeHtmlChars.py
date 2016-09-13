602f8704657d55f187a166a406e401d401a3ac67
shelbyt <sht005@eng.ucsd.edu>
2016-09-12 22:20:01 -0700

    Updated example to remove br tags

--- helper/escapeHtmlChars.py
+++ helper/escapeHtmlChars.py
@@ -1,5 +1,9 @@
+# Use this to strip the unicode chars
 import HTMLParser
 parser = HTMLParser.HTMLParser()
+# Use this to strip the <br> chars
+import re
 with open('output.en.ttml') as infile, open('pro-output.ttml', 'w') as outfile:
     for line in infile:
+        line = re.sub('<br.*?/>', ' ', line)
         outfile.write(parser.unescape(line))
